a0f3a8913b1b5d1f00a050980e6e1873778705ab
huhx <gohuhx@163.com>
2024-06-11 22:26:32 +0800

    feat: rename util string method

--- util/string.go
+++ util/string.go
@@ -2,11 +2,11 @@ package util
 
 import "strconv"
 
-func String2Int64(text string) int64 {
+func StringToInt64(text string) int64 {
 	number, _ := strconv.ParseInt(text, 10, 64)
 	return number
 }
 
-func Int642String(number int64) string {
+func Int64ToString(number int64) string {
 	return strconv.FormatInt(number, 10)
 }
