2e4ba657e21a4fe7d3d2a4c96f3d9b5280a519ba
huhx <gohuhx@163.com>
2024-12-15 07:48:55 +0800

    [huhx] -feat: add jsonb types

--- go.mod
+++ go.mod
@@ -1,12 +1,17 @@
 module github.com/huhx/common-go
 
-go 1.22
+go 1.22.0
+
+toolchain go1.23.4
 
 require (
-	github.com/6tail/lunar-go v1.3.13
+	github.com/6tail/lunar-go v1.3.15
 	github.com/bwmarrin/snowflake v0.3.0
-	github.com/goccy/go-json v0.10.3
+	github.com/goccy/go-json v0.10.4
 	go.uber.org/zap v1.27.0
 )
 
-require go.uber.org/multierr v1.11.0 // indirect
+require (
+	github.com/stretchr/testify v1.10.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+)
---
+++ types/jsonb.go
@@ -0,0 +1,28 @@
+package types
+
+import (
+	"database/sql/driver"
+	"errors"
+	"github.com/goccy/go-json"
+)
+
+// JSONB Interface for JSONB Field of yourTableName Table
+type JSONB map[string]interface{}
+
+func NewJSONB(data map[string]interface{}) JSONB {
+	return data
+}
+
+// Value Marshal
+func (a JSONB) Value() (driver.Value, error) {
+	return json.Marshal(a)
+}
+
+// Scan Unmarshal
+func (a *JSONB) Scan(value interface{}) error {
+	b, ok := value.([]byte)
+	if !ok {
+		return errors.New("type assertion to []byte failed")
+	}
+	return json.Unmarshal(b, &a)
+}
