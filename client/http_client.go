32f676e71db3f977d2eee14c8ebc527568cc0a93
huhx <gohuhx@163.com>
2025-02-01 21:14:47 +0800

    [huhx] -feat: add get request

--- client/http_client.go
+++ client/http_client.go
@@ -7,6 +7,7 @@ import (
 	"github.com/goccy/go-json"
 	"io"
 	"net/http"
+	"net/url"
 )
 
 type HttpClient struct {
@@ -22,6 +23,41 @@ func NewTLSHttpClient() HttpClient {
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
 	return HttpClient{Client: http.Client{Transport: transport}}
+}
+
+func (c *HttpClient) Get(baseUrl string, params map[string]string, header http.Header) (*Response, error) {
+	values := url.Values{}
+	for key, value := range params {
+		values.Add(key, value)
+	}
+
+	fullURL := baseUrl + "?" + values.Encode()
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return nil, err
+	}
+	req.Header = header
+
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return nil, err
+	}
+
+	return &Response{
+		Status:     resp.Status,
+		StatusCode: resp.StatusCode,
+		Header:     resp.Header,
+		Body:       body,
+	}, nil
 }
 
 func (c *HttpClient) Post(url string, payload interface{}, header http.Header) (*Response, error) {
