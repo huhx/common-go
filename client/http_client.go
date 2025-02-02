ff52bad5d60a57481212b2dd03a7c0e02777cf8d
huhx <gohuhx@163.com>
2025-02-02 14:47:49 +0800

    [huhx] -feat: add put and delete common http request

--- client/http_client.go
+++ client/http_client.go
@@ -60,14 +60,78 @@ func (c *HttpClient) Get(baseUrl string, params map[string]string, header http.H
 	}, nil
 }
 
-func (c *HttpClient) Post(url string, payload interface{}, header http.Header) (*Response, error) {
+func (c *HttpClient) Post(baseUrl string, payload interface{}, header http.Header) (*Response, error) {
 	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
 		fmt.Println("Error marshalling payload:", err)
 		return nil, err
 	}
 
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(payloadBytes))
+	req, err := http.NewRequest("POST", baseUrl, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return nil, err
+	}
+	req.Header = header
+
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return nil, err
+	}
+
+	return &Response{
+		Status:     resp.Status,
+		StatusCode: resp.StatusCode,
+		Header:     resp.Header,
+		Body:       body,
+	}, nil
+}
+
+func (c *HttpClient) Put(urlPath string, payload interface{}, header http.Header) (*Response, error) {
+	payloadBytes, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println("Error marshalling payload:", err)
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", urlPath, bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		fmt.Println("Error creating request:", err)
+		return nil, err
+	}
+	req.Header = header
+
+	resp, err := c.Client.Do(req)
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return nil, err
+	}
+
+	return &Response{
+		Status:     resp.Status,
+		StatusCode: resp.StatusCode,
+		Header:     resp.Header,
+		Body:       body,
+	}, nil
+}
+
+func (c *HttpClient) Delete(urlPath string, header http.Header) (*Response, error) {
+	req, err := http.NewRequest("DELETE", urlPath, nil)
 	if err != nil {
 		fmt.Println("Error creating request:", err)
 		return nil, err
