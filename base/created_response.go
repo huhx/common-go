ad17bcab8473eae4eaf9353aa0913a9ca99d0ef1
huhx <gohuhx@163.com>
2025-06-07 09:44:08 +0800

    [huhx] -feat: add string created response

--- base/created_response.go
+++ base/created_response.go
@@ -9,3 +9,11 @@ type CreatedResponse struct {
 type BatchCreatedResponse struct {
 	Ids types.Int64Array `json:"ids" example:"1800783867820785152,1800783867820785153"`
 }
+
+type CreatedStringResponse struct {
+	Id string `json:"id" example:"1800783867820785152"`
+}
+
+type BatchCreatedStringResponse struct {
+	Ids types.StringArray `json:"ids" example:"1800783867820785152,1800783867820785153"`
+}
